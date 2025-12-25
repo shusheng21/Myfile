8d9f09d938f32ab8ca035dbb6cd991302eda0020
shusheng21 <aw20011021@163.com>
2025-12-25 09:14:58 +0800

    2025-12-25  9:14:58.68

--- Code/Fast_java/MavenProject/TestSpringMVC/src/main/java/com/rao/controller/TestController.java
+++ Code/Fast_java/MavenProject/TestSpringMVC/src/main/java/com/rao/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.rao.controller;
 
+import com.rao.pojo.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,6 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @RequestMapping("/test1")
     public String test1(){
+        return "index.jsp";
+    }
+
+    //访问路径http://localhost:8887/testspringmvc/testParam?name=marry&age=18
+    //url中的name和age变量要和方法中的参数名对应
+    @RequestMapping("/testParam")
+    public String testParam(String name,int age){
+        System.out.println(name + "----" + age);
+        return "index.jsp";
+    }
+
+    //访问路径http://localhost:8887/testspringmvc/testParam?name=marry&age=18
+    //url中的name和age变量要和Person类中的变量名对应
+    @RequestMapping("/testParam23") //这里面的路径"/testParam23"随便写，不要求和方法名一样
+    public String testParam2(Person p){
+        System.out.println(p.getName() + "----" + p.getAge());
         return "index.jsp";
     }
 }
